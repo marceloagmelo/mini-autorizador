b2afc2b3f2ff7149cc2cf9b01242dc79d3fab989
Marcelo Melo <marceloagmelo@gmail.com>
2024-12-12 08:39:38 -0300

    Refatoração para separar os controllers

--- src/main/java/com/br/vr/miniautorizador/controller/CartaoController.java
+++ src/main/java/com/br/vr/miniautorizador/controller/CartaoController.java
@@ -3,9 +3,6 @@ package com.br.vr.miniautorizador.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.vr.miniautorizador.entity.Cartao;
-import com.br.vr.miniautorizador.entity.MensagemErro;
-import com.br.vr.miniautorizador.exception.SaldoInsuficientException;
-import com.br.vr.miniautorizador.exception.SenhaInvalidaException;
 import com.br.vr.miniautorizador.service.CartaoService;
 
 import java.io.IOException;
@@ -17,25 +14,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/cartoes")
 public class CartaoController {
 
     @Autowired
-    CartaoService cartaoService;
+    private CartaoService cartaoService;
 
-    @PostMapping("/cartoes")
+    @PostMapping
     public ResponseEntity<?> criar(@RequestBody Cartao cartao) throws IOException {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(cartaoService.criarCartao(cartao));
         } catch (IOException ioe) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
     }
 
-    @GetMapping("/cartoes/{numeroCartao}")
+    @GetMapping("/{numeroCartao}")
     public ResponseEntity<?> obterSaldo(@PathVariable String numeroCartao) throws IOException {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(cartaoService.getCartao(numeroCartao));
@@ -45,23 +44,6 @@ public class CartaoController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
 
-    }
-
-    @PostMapping("/transacoes")
-    public ResponseEntity<?> transacoes(@RequestBody Cartao cartao) throws IOException {
-        try {
-            return ResponseEntity.status(HttpStatus.OK).body(cartaoService.efetuarTransacao(cartao));
-        } catch (SenhaInvalidaException esi) {
-            MensagemErro erro = new MensagemErro("Senha invalida");
-            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(erro);
-        } catch (SaldoInsuficientException esi) {
-            MensagemErro erro = new MensagemErro("Saldo insuficiente");
-            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(erro);
-        } catch (IOException ioe) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
     }
 
 }
